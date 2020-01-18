a9431939a528fd0f863f6fd75cb94cf537ea0076
gainstrive <gainstrive@gmail.com>
2020-01-18 09:47:08 -0500

    Added process.env.

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 
 app.use("/api", UserRoutes)
 
-mongoose.connect("mongodb://127.0.0.1:27017/jsrpg-react", {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/jsrpg-react", {useNewUrlParser: true});
 const connection = mongoose.connection;
 
 connection.once("open", function() {
